809824e0e4ec06a5566951694a9ab9f8669b8f05
Samuel <samuelspeed574@gmail.com>
2025-10-02 22:05:55 -0400

    Frontend actualizado con cambios en css para ser reconocido por tailwind

--- FRONTEND/FlyPyApp/src/App.tsx
+++ FRONTEND/FlyPyApp/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import PaginaDeRegistro from "./pages/PaginaDeRegistro";
 
 function App() {
--- FRONTEND/FlyPyApp/src/components/FormularioDeRegistro.tsx
+++ FRONTEND/FlyPyApp/src/components/FormularioDeRegistro.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 const FormularioDeRegistro = () => {
   return (
-    <form className="bg-white shadow-md rounded p-6 w-96">
+    <form className="bg-red shadow-md rounded p-6 w-96">
       <h2 className="text-2xl font-bold mb-6 text-center">Crear Cuenta</h2>
 
       {/* Nombre */}
