4af3dfaa89ba62baa1e8e286c17c1491cece91ad
Samuel <samuelspeed574@gmail.com>
2025-10-07 23:54:23 -0400

    Pagina inicial de crear cursos

--- FRONTEND/FlyPyApp/src/App.tsx
+++ FRONTEND/FlyPyApp/src/App.tsx
@@ -1,7 +1,14 @@
-import PaginaDeRegistro from "./pages/PaginaDeRegistro";
+/*import PaginaDeRegistro from "./pages/PaginaDeRegistro";
 
 function App() {
   return <PaginaDeRegistro />;
+}
+
+export default App;*/
+import FormularioCrearCurso from "./components/FormularioCrearCurso";
+
+function App() {
+  return <FormularioCrearCurso />;
 }
 
 export default App;
